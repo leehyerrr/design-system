f8a3170393286b2d044eeb1dba746fca4a8e3206
leehyerrr <leehyerrr@gmail.com>
2026-08-10 07:48:48 +0900

    #컬러토큰 css

--- packages/themes/scripts/build-css-module.js
+++ packages/themes/scripts/build-css-module.js
@@ -1,6 +1,10 @@
 import fs from "fs";
 import * as theme from "../dist/index.js";
 
+const generateThemeCssVariables = () => {
+  const cssString = [];
+};
+
 Object.entries(theme.vars).forEach(([key, value]) => {
   console.log(key, value.$static);
 });
