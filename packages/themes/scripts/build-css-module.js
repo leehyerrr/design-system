a1cf4770977b343b14db296a562df38f6b28de11
leehyerrr <leehyerrr@gmail.com>
2026-08-22 13:59:10 +0900

    #모노레포 - workspce 설정

--- packages/themes/scripts/build-css-module.js
+++ packages/themes/scripts/build-css-module.js
@@ -1,12 +1,50 @@
 import fs from "fs";
 import * as theme from "../dist/index.js";
 
-const generateThemeCssVariables = () => {
-  const cssString = [];
+// theme.css
+// :root {
+//   --gray-900: #171923
+// }
+
+const toCssCasting = (str) => {
+  return str
+    .replace(/([a-z])(\d)/, "$1-$2")
+    .replace(/([A-Z])/g, "-$1")
+    .toLowerCase();
 };
 
-Object.entries(theme.vars).forEach(([key, value]) => {
-  console.log(key, value.$static);
-});
+const generateThemeCssVariables = () => {
+  const cssString = [];
 
-fs.writeFileSync("dist/themes.css", "");
+  Object.entries(theme.vars).forEach(([key, value]) => {
+    if (key === "colors") {
+      Object.entries(value.$static).forEach(([colorKey, colorValue]) => {
+        if (colorKey === "light") {
+          const selector = ":root";
+
+          const cssVariables = Object.entries(colorValue)
+            .map(([mainKey, mainValue]) =>
+              Object.entries(mainValue)
+                .map(
+                  ([subKey, subValue]) =>
+                    `--${toCssCasting(mainKey)}-${toCssCasting(subKey)}: ${subValue};`,
+                )
+                .join("\n"),
+            )
+            .join("\n");
+
+          cssString.push(`${selector} {\n${cssVariables}\n}`);
+        }
+      });
+    }
+  });
+  return cssString;
+};
+
+const generateThemeCss = () => {
+  const variables = generateThemeCssVariables();
+
+  fs.writeFileSync("dist/themes.css", [...variables].join("\n"));
+};
+
+generateThemeCss();
