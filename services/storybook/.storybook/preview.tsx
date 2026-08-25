d844534f0356923f1434eee41d8653b5cba501f0
leehyerrr <leehyerrr@gmail.com>
2026-08-25 22:28:04 +0900

    #storybook dark/light theme 적용

--- services/storybook/.storybook/preview.tsx
+++ services/storybook/.storybook/preview.tsx
@@ -59,4 +59,24 @@ const preview: Preview = {
   ],
 };
 
+// const initTheme = () => {
+//   const isDarkTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+//   if (isDarkTheme) {
+//     document.body.classList.add("theme-dark");
+//   }
+
+//   const mediaQueryList = window.matchMedia("(prefers-color-scheme: dark)");
+
+//   mediaQueryList.addEventListener("change", (e) => {
+//     if (e.matches) {
+//       document.body.classList.add("theme-dark");
+//     } else {
+//       document.body.classList.remove("theme-dark");
+//     }
+//   });
+// };
+
+// initTheme();
+
 export default preview;
